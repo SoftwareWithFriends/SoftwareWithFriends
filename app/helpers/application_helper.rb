1e3a2e4970a2ebedfbd1af5506cc269ccf3fa873
perf <perf@skarven.net>
2011-09-20 15:16:38 -0400

    [ryan] added partials for header and nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def navigation_elements
+    elements = {}
+
+  end
+
 end
+
+    #%li.page Development
+    #%li.filter Features
+    #%li.filter Bugs
+    #%li.filter Design
+    #%li.page Projects
+    #%li.filter Active Metric
+    #%li.filter Ranges
+    #%li.filter Things
+    #%li.page Authors
