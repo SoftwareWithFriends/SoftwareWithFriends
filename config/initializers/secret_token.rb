ee5a52452ffe1b3c43ccb61423fb941378559c8b
ryan mcgarvey <mcgarvey.ryan@gmail.com>
2011-12-17 14:16:34 -0500

    [ryan] initial commit. rails new sfw -O

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swf::Application.config.secret_token = 'e1e4a3f5680bf193dc26171da1b0bb97866cc8cd29538f112619f94c6d9baaf767de61f751b95d15f9349e18866382474629d503d2591639449d9f5412f3f311'
